8d489167a45db23666efbc48006a244cc2ecbc4c
kiani kalyan <parisa.kiani70@gmail.com>
2018-12-25 15:26:26 +0330

    changed book controller 4-9-97

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Book extends MY_Controller
 {
-    //TEST test
+    //TEST test1
     function __construct()
     {
         parent::__construct();
